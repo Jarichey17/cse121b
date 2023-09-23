1c07c658862390752d9b4f3dff8eaef82aed2476
Jarichey17 <ric20041@byui.edu>
2023-09-23 15:13:12 -0600

    fix so picture shows

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -11,7 +11,7 @@ let profilePicture = 'images/wedding.jpg'
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.getElementById('profile-image');
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
